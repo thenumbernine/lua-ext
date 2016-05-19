17f44ae11276417f49da14254ab3907c1bd542d4
Christopher Moore <christopher.e.moore@gmail.com>
2016-05-19 10:50:05 +0300

    more reorganizing.  made use of state table in place of closures.  allowed table serialization to be overridden (before it was the only type that wasn't).

--- tolua.lua
+++ tolua.lua
@@ -58,12 +58,32 @@ local function escapeString(s)
 	return ('%q'):format(s):gsub('\\\n','\\n')
 end
 
+-- returns 'true' if k is a valid variable name
+local function isVarName(k)
+	return type(k) == 'string' and k:match('^[_,a-z,A-Z][_,a-z,A-Z,0-9]*$')
+end
+
+local toLuaRecurse
+
+local function toLuaKey(state, k, path)
+	if isVarName(k) then
+		return k, true
+	else
+		local result = toLuaRecurse(state, k, nil, path, true)
+		if result then
+			return '['..result..']', false
+		else
+			return false, false
+		end
+	end
+end
+
 local defaultSerializeForType = {
-	number = tostring,
-	boolean = tostring,
-	['nil'] = tostring,
-	string = escapeString,
-	['function'] = function(x)
+	number = function(state,x) return tostring(x) end,
+	boolean = function(state,x) return tostring(x) end,
+	['nil'] = function(state,x) return tostring(x) end,
+	string = function(state,x) return escapeString(x) end,
+	['function'] = function(state, x)
 		local result, s = pcall(string.dump, x)
 		
 		if result then
@@ -97,157 +117,144 @@ local defaultSerializeForType = {
 			
 		return s
 	end,
-}
-
--- returns 'true' if k is a valid variable name
-local function isVarName(k)
-	return type(k) == 'string' and k:match('^[_,a-z,A-Z][_,a-z,A-Z,0-9]*$')
-end
-
-local function tolua(x, args)
-	local indentChar = ''
-	local newlineChar = ''
-	local serializeForType 
-	local serializeMetatables
-	if args then
-		if args.indent then
-			indentChar = '\t'
-			newlineChar = '\n'
-		end
-		serializeForType = args.serializeForType 
-		serializeMetatables = args.serializeMetatables
-	end	
-	
-	local wrapWithFunction = false
-	local recursiveReferences = table()
-	local touchedTables = {}
-
-	local toLuaRecurse
-	local function toLuaKey(k, path)
-		if isVarName(k) then
-			return k, true
-		else
-			local result = toLuaRecurse(k, nil, path, true)
-			if result then
-				return '['..result..']', false
-			else
-				return false, false
-			end
-		end
-	end
-	
-	toLuaRecurse = function(x, tab, path, keyRef)
-		if not tab then tab = '' end
-		local newtab = tab .. indentChar
-		local xtype = type(x)
+	table = function(state, x, tab, path, keyRef)
 		local result
-		if xtype == 'table' then
-			-- TODO override for specific metatables?  as I'm doing for types?
+
+		local newtab = tab .. state.indentChar
+		-- TODO override for specific metatables?  as I'm doing for types?
+		
+		if state.touchedTables[x] then
+			result = false	-- false is used internally and means recursive reference
+			state.wrapWithFunction = true
 			
-			if touchedTables[x] then
-				result = false	-- false is used internally and means recursive reference
-				wrapWithFunction = true
-				
-				-- we're serializing *something*
-				-- is it a value?  if so, use the 'path' to dereference the key
-				-- is it a key?  if so the what's the value ..
-				-- do we have to add an entry for both?
-				-- maybe the caller should be responsible for populating this table ...
-				if keyRef then
-					recursiveReferences:insert('root'..path..'['..touchedTables[x]..'] = error("can\'t handle recursive references in keys")')
-				else 
-					recursiveReferences:insert('root'..path..' = '..touchedTables[x])
-				end
-			else
-				touchedTables[x] = 'root'..path
-				
-				-- prelim see if we can write it as an indexed table
-				local numx = maxn(x)
-				local intNilKeys, intNonNilKeys = 0, 0				
-				for i=1,numx do
-					if x[i] == nil then
-						intNilKeys = intNilKeys + 1
-					else
-						intNonNilKeys = intNonNilKeys + 1
-					end
-				end
-
-				local s = table()
-				
-				-- add integer keys without keys explicitly. nil-padded so long as there are 2x values than nils
-				local addedIntKeys = {}
-				if intNonNilKeys >= intNilKeys * 2 then	-- some metric
-					for k=1,numx do
-						local nextResult = toLuaRecurse(x[k], newtab, path and path..'['..k..']')
-						if nextResult then
-							s:insert(nextResult)
-						-- else x[k] is a recursive reference 
-						end
-						addedIntKeys[k] = true
-					end
-				end
-
-				-- sort key/value pairs added here by key
-				local mixed = table()
-				for k,v in pairs(x) do
-					if not addedIntKeys[k] then
-						local keyStr, usesDot = toLuaKey(k, path)
-						if keyStr then
-							local newpath
-							if path then
-								newpath = path 
-								if usesDot then newpath = newpath .. '.' end
-								newpath = newpath .. keyStr
-							end
-							local nextResult = toLuaRecurse(v, newtab, newpath)
-							if nextResult then
-								mixed:insert{keyStr, nextResult}
-							-- else x[k] is a recursive reference
-							end
-						end
-					end
-				end
-				mixed:sort(function(a,b) return a[1] < b[1] end)	-- sort by keys
-				mixed = mixed:map(function(kv) return table.concat(kv, '=') end)
-				s:append(mixed)
-
-				local rs = '{'..newlineChar
-				if #s > 0 then rs = rs .. newtab ..s:concat(','..newlineChar..newtab) .. newlineChar end
-				rs = rs .. tab.. '}'
-				result = rs
+			-- we're serializing *something*
+			-- is it a value?  if so, use the 'path' to dereference the key
+			-- is it a key?  if so the what's the value ..
+			-- do we have to add an entry for both?
+			-- maybe the caller should be responsible for populating this table ...
+			if keyRef then
+				state.recursiveReferences:insert('root'..path..'['..state.touchedTables[x]..'] = error("can\'t handle recursive references in keys")')
+			else 
+				state.recursiveReferences:insert('root'..path..' = '..state.touchedTables[x])
 			end
 		else
-			local serializeFunction
-			if serializeForType then
-				serializeFunction = serializeForType[xtype]
+			state.touchedTables[x] = 'root'..path
+			
+			-- prelim see if we can write it as an indexed table
+			local numx = maxn(x)
+			local intNilKeys, intNonNilKeys = 0, 0				
+			for i=1,numx do
+				if x[i] == nil then
+					intNilKeys = intNilKeys + 1
+				else
+					intNonNilKeys = intNonNilKeys + 1
+				end
 			end
-			if not serializeFunction then
-				serializeFunction = defaultSerializeForType[xtype]
+
+			local s = table()
+			
+			-- add integer keys without keys explicitly. nil-padded so long as there are 2x values than nils
+			local addedIntKeys = {}
+			if intNonNilKeys >= intNilKeys * 2 then	-- some metric
+				for k=1,numx do
+					local nextResult = toLuaRecurse(state, x[k], newtab, path and path..'['..k..']')
+					if nextResult then
+						s:insert(nextResult)
+					-- else x[k] is a recursive reference 
+					end
+					addedIntKeys[k] = true
+				end
 			end
-			if serializeFunction then
-				result = serializeFunction(x)
-			else
-				result = '['..type(x)..':'..tostring(x)..']'
+
+			-- sort key/value pairs added here by key
+			local mixed = table()
+			for k,v in pairs(x) do
+				if not addedIntKeys[k] then
+					local keyStr, usesDot = toLuaKey(state, k, path)
+					if keyStr then
+						local newpath
+						if path then
+							newpath = path 
+							if usesDot then newpath = newpath .. '.' end
+							newpath = newpath .. keyStr
+						end
+						local nextResult = toLuaRecurse(state, v, newtab, newpath)
+						if nextResult then
+							mixed:insert{keyStr, nextResult}
+						-- else x[k] is a recursive reference
+						end
+					end
+				end
 			end
-		end
-		assert(result ~= nil)
-		if serializeMetatables then
-			local m = getmetatable(x)
-			if m then
-				result = 'setmetatable('..result..', '..toLuaRecurse(m, newtab)..')'
-			end
+			mixed:sort(function(a,b) return a[1] < b[1] end)	-- sort by keys
+			mixed = mixed:map(function(kv) return table.concat(kv, '=') end)
+			s:append(mixed)
+
+			local rs = '{'..state.newlineChar
+			if #s > 0 then rs = rs .. newtab ..s:concat(','..state.newlineChar..newtab) .. state.newlineChar end
+			rs = rs .. tab.. '}'
+			result = rs
 		end
 		return result
-	end
+	end,
+}
 
-	local str = toLuaRecurse(x, nil, '')
+toLuaRecurse = function(state, x, tab, path, keyRef)
+	if not tab then tab = '' end
 	
-	if wrapWithFunction then
-		str = '(function()' .. newlineChar
-			.. indentChar .. 'local root = '..str .. ' ' .. newlineChar
+	local xtype = type(x)
+	local serializeFunction
+	if state.serializeForType then
+		serializeFunction = state.serializeForType[xtype]
+	end
+	if not serializeFunction then
+		serializeFunction = defaultSerializeForType[xtype]
+	end
+	
+	local result
+	if serializeFunction then
+		result = serializeFunction(state, x, tab, path, keyRef)
+	else
+		result = '['..type(x)..':'..tostring(x)..']'
+	end
+	assert(result ~= nil)
+	
+	if state.serializeMetatables then
+		local m = getmetatable(x)
+		if m then
+			result = 'setmetatable('..result..', '..toLuaRecurse(state, m, tab..state.indentChar)..')'
+		end
+	end
+	
+	return result
+end
+
+
+local function tolua(x, args)
+	local state = {
+		indentChar = '',
+		newlineChar = '',
+		wrapWithFunction = false,
+		recursiveReferences = table(),
+		touchedTables = {},
+	}
+	if args then
+		if args.indent then
+			state.indentChar = '\t'
+			state.newlineChar = '\n'
+		end
+		state.serializeForType = args.serializeForType 
+		state.serializeMetatables = args.serializeMetatables
+	end	
+	
+	local str = toLuaRecurse(state, x, nil, '')
+	
+	if state.wrapWithFunction then
+		str = '(function()' .. state.newlineChar
+			.. state.indentChar .. 'local root = '..str .. ' ' .. state.newlineChar
 			-- TODO defer self-references to here
-			.. recursiveReferences:concat(' '..newlineChar..indentChar) .. ' ' .. newlineChar
-			.. indentChar .. 'return root ' .. newlineChar
+			.. state.recursiveReferences:concat(' '..state.newlineChar..state.indentChar) .. ' ' .. state.newlineChar
+			.. state.indentChar .. 'return root ' .. state.newlineChar
 			.. 'end)()'
 	end
 	
@@ -255,15 +262,15 @@ local function tolua(x, args)
 end
 
 return setmetatable({}, {
-	__call = function(self, ...)
-		return tolua(...)
+	__call = function(self, x, args)
+		return tolua(x, args)
 	end,
 	__index = {
-		-- the function itself
-		tolua = tolua,
 		-- escaping a Lua string for load() to use
 		escapeString = escapeString,
 		-- returns 'true' if the key passed is a valid Lua variable string, 'false' otherwise
 		isVarName = isVarName,
+		-- table of default serialization functions indexed by each time
+		defaultSerializeForType = defaultSerializeForType,
 	}
 })
