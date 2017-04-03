f26904ba5a74dc82fd3af9e2425a17e0eb9ab396
Christopher Moore <christopher.e.moore@gmail.com>
2017-04-03 19:46:24 +0100

    typo

--- os.lua
+++ os.lua
@@ -8,7 +8,7 @@ if _VERSION == 'Lua 5.1' then
 		local errcode = execute(cmd)
 		local reason = ({
 			[0] = 'exit',
-		})[errcode] or 'unknown',
+		})[errcode] or 'unknown'
 		return errcode==0 and true or nil, reason, errcode
 	end
 end
