72e4e89f86925ce1a98e26d095ce4f1e0c4dc27b
Christopher Moore <christopher.e.moore@gmail.com>
2021-06-24 02:19:56 +0600

    comments

--- op.lua
+++ op.lua
@@ -1,6 +1,17 @@
+--[[
+make lua functions for each operator.
+it looks like i'm mapping 1-1 between metamethods and fields in this table.
+useful for using Lua as a functional language.
+--]]
+
+-- test if we hae lua 5.3 bitwise operators
+-- orrr I could just try each op and bail out on error
+-- and honestly I should be defaulting to the 'bit' library anyways, esp in the case of luajit where it is translated to an asm opcode
 local lua53 = _VERSION >= 'Lua 5.3'
 
 local symbolscode = [[
+	
+	-- which fields are unary operators
 	local unary = {
 		unm = true,
 		bnot = true,
@@ -34,7 +45,31 @@ if lua53 then
 		shl = '<<',			-- 5.3
 		shr = '>>',			-- 5.3
 		bnot = '~',			-- 5.3, unary
-]]	
+]]
+--[[ alternatively, luajit 'bit' library:
+I should probably include all of these instead
+would there be a perf hit from directly assigning these functions to my own table,
+ as there is a perf hit for assigning from ffi.C func ptrs to other variables?  probably.
+ how about as a tail call / vararg forwarding?
+I wonder if luajit adds extra metamethods
+
+luajit 2.0		lua 5.2		lua 5.3
+band			band		&
+bnot			bnot		~
+bor				bor			|
+bxor			bxor		~
+lshift			lshift		<<
+rshift			rshift		>>
+arshift			arshift
+rol				lrotate
+ror				rrotate
+bswap (reverses 32-bit integer endian-ness of bytes)
+tobit (converts from lua number to its signed 32-bit value) 
+tohex (string conversion)
+				btest (does some bitflag stuff)
+				extract (same)
+				replace (same)
+--]]
 end
 symbolscode = symbolscode .. [[
 	}
