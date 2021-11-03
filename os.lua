927550b91bf1186aac2121e19e0ce243db771fcc
Christopher Moore <christopher.e.moore@gmail.com>
2021-11-03 09:04:42 +0600

    worked on windows, not so much on linux

--- os.lua
+++ os.lua
@@ -36,11 +36,15 @@ local function windows()
 	if ffi then
 		return ffi.os == 'Windows'
 	else
-		-- TODO what if uname doesn't exist? does it exist in Windows?
+		-- TODO what if uname doesn't exist? then this will output to stderr.  does it exist in Windows?
+		-- to get around that on Windows I can pipe to > NUL
+		-- TODO what if it's not Windows?  then this will create a NUL file ...
+		-- honestly I could just use the existence of piping to NUL vs /dev/null to determine Windows vs Unix ...
 		return ({
 			msys = true,
 			ming = true,
-		})[(io.popen'uname 2> NUL':read'*a'):sub(1,4):lower()]
+		--})[(io.popen'uname 2> NUL':read'*a'):sub(1,4):lower()]
+		})[(io.popen'uname':read'*a'):sub(1,4):lower()]
 	end
 end
 
