3e85803865fb58aff12a918b60baa872615a4a8d
Christopher Moore <christopher.e.moore@gmail.com>
2022-05-28 17:09:58 +0600

    in case I forget why I wrote this again

--- coroutine.lua
+++ coroutine.lua
@@ -4,6 +4,15 @@ for k,v in pairs(require 'coroutine') do coroutine[k] = v end
 local function safehandle(thread, res, ...)
 	if not res then
 		local err = tostring(...)..'\n'..debug.traceback(thread)
+		--[[
+		reminder to myself:
+		this is here because where else should it go?
+		I don't want assertresume to error, but I do want it to print a stacktrace
+		it is especially used with threadmanager, which calls assertresume, then gets the status to determine if the thread is running or not
+		I could put an assert() around that, but assert(assertresume()) seems strange, and I'd still have to only print the exceptions when the status ~= dead
+		So things seem to work best with threadmanager if I put this here.
+		Maybe I shouldn't call it 'assertresume' but instead something else like 'resumeAndPrintErrorIfThereIsOne' ?
+		--]]
 		io.stderr:write(err..'\n')
 		io.stderr:flush()
 		return false, err
