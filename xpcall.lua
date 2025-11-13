b2d2ca641ae56d9eb24c2d39d4caaf93bc280c71
Christopher Moore <christopher.e.moore@gmail.com>
2025-11-13 12:51:45 +0700

    changed ext.xpcall to insert  a default error handler

--- xpcall.lua
+++ xpcall.lua
@@ -3,27 +3,53 @@ shim layer for xpcall if it doesn't support ... forwarding
 used by ext.load
 xpcall doesn't forward extra args in vanilla lua 5.1, and in luajit without 5.2 compat (I think?)
 
-TODO tempting to always insert a new xpcall and give it a default error-handler of `function(err) return err..'\n'..debug.traceback() end`
- so that it will always capture the stack trace by default.
+while I'm here I will insert a default error handler, since 99% of my xpcall functions do nothing more than
+`function(err) return err..'\n'..debug.traceback() end`
+ so that it will always capture the stack trace at the error by default.
 --]]
+
+local function defaultErrorHandler(err)
+	return tostring(err)..'\n'..debug.traceback()
+end
+
 return function(env)
 	env = env or _G
 	local oldxpcall = env.xpcall or _G.xpcall
-	local xpcallfwdargs = select(2,
+	local xpcallCanFwdArgs = select(2,
 		oldxpcall(
 			function(x) return x end,
 			function() end,
 			true
 		)
 	)
-	if xpcallfwdargs then return end
-	local unpack = env.unpack or table.unpack
-	local function newxpcall(f, err, ...)
-		local args = {...}
-		args.n = select('#', ...)
-		return oldxpcall(function()
-			return f(unpack(args, 1, args.n))
-		end, err)
+
+	-- perform an xpcall without an error handler
+	-- see if it errors
+	local xpcallHasDefaultErrorHandler = xpcall(function()
+		xpcall(function() end)
+	end, function() end)
+	if xpcallCanFwdArgs then
+		if not xpcallHasDefaultErrorHandler then
+			-- xpcall arg forwarding works
+			-- just replace xpcall with a default error handler
+			local newxpcall = function(f, err, ...)
+				err = err or defaultErrorHandler
+				return oldxpcall(f, err, ...)
+			end
+			env.xpcall = newxpcall
+		end
+	else
+		-- xpcall arg forwarding doesn't work
+		-- replace it with an xpcall that does arg forwarding
+		local unpack = env.unpack or table.unpack
+		local newxpcall = function(f, err, ...)
+			err = err or defaultErrorHandler
+			local args = {...}
+			args.n = select('#', ...)
+			return oldxpcall(function()
+				return f(unpack(args, 1, args.n))
+			end, err)
+		end
+		env.xpcall = newxpcall
 	end
-	env.xpcall = newxpcall
 end
